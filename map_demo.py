e8c1358e845d9143bb69a79181a38870bc44dc0c
SarahHeidekorn <56751586+SarahHeidekorn@users.noreply.github.com>
2023-03-02 11:13:00 +0100

    Update map_demo.py

--- map_demo.py
+++ map_demo.py
@@ -1,70 +1,65 @@
 
 import streamlit as st
 import folium
-import pandas as pd 
-import plotly.express as px
+import pandas as pd
 from streamlit_folium import st_folium
 
-st.set_page_config(page_title = "Map Demo", layout = "wide")    
-
+# Create a dictionary of base maps with their names and tiles
 base_maps = {
     "OpenStreetMap": folium.TileLayer("cartodbpositron"),
-    "Terrain": folium.TileLayer("Stamen Terrain"),
-    "Toner": folium.TileLayer("Stamen Toner"),
-    "Watercolor": folium.TileLayer("Stamen Watercolor"),
+    "Stamen Terrain": folium.TileLayer("Stamen Terrain"),
+    "Stamen Toner": folium.TileLayer("Stamen Toner"),
+    "Stamen Watercolor": folium.TileLayer("Stamen Watercolor"),
 }
-
+   
 st.title("Map Demo Test")
 st.write("The map below is supposed to display a potential implementation of cartographic visualizations for an example data set.")
 st.sidebar.header("Select Options")
 
-
-#@st.cache_data() 
+@st.cache_data() 
 def load_data():
     df = pd.read_csv(file)
     return df
 
-file = "test_data.csv"
+file = "cleaned_data.csv"
 df = load_data()
 df.rename(columns={'Lithology/Mineral' : 'Lithology_Mineral'}, inplace=True)
 
-filter_form = st.sidebar.form(key="Options") #create container to optimize functionality
-
-#set filter options
-location_type = filter_form.multiselect(
+form1 = st.sidebar.form(key="Options")
+location_type = form1.multiselect(
     "Choose the location type:",
     options =df["Location Type"].unique()
 )
-material = filter_form.multiselect(
+
+material = form1.multiselect(
     "Choose the lithology/mineral",
     options = df["Lithology_Mineral"].unique()
 )
-sample = filter_form.multiselect(
+
+sample = form1.multiselect(
     "Choose the sample",
     options = df["Sample"].unique()
-)    
-data_package = filter_form.multiselect(
+)
+
+data_package = form1.multiselect(
     "Choose the data package",
     options = df["Data Package"].unique()
-)    
- 
+)
+
 location_type_str = '|'.join(location_type)
 material_str = '|'.join(material)
 sample_str = '|'.join(sample)
 data_package_str = '|'.join(data_package)
 
-# filter dataframe using str.contains
+# Filter dataframe using str.contains
 df_selection = df[df['Location Type'].str.contains(location_type_str) & 
                   df['Lithology_Mineral'].str.contains(material_str) & 
                   df['Sample'].str.contains(sample_str) & 
                   df['Data Package'].str.contains(data_package_str)]
 
-
 st.dataframe(df_selection)
-filter_form.form_submit_button("Submit")
+form1.form_submit_button("Submit")
 
-
-# construction of the interactive map:
 df_2 = df_selection.dropna(subset=['Longitude'])
 df_2 = df_selection.dropna(subset=['Latitude'])
 
